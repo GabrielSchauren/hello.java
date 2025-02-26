7212ae29cbe3c09115be695416087613643a9b93
GabrielSchauren <gajschauren@minha.fag.edu.br>
2025-02-26 01:24:23 -0300

    criado o ola mundo

--- main.java
+++ main.java
@@ -1,8 +1,12 @@
 public class main {
     public static void main(String[] args) {
         HelloWorld();
+        olamundo();
     }
     public static void HelloWorld() {
         System.out.println("Hello World!");
+    }
+    public static void olamundo() {
+        System.out.println("Olá Mundo!");
     }
 }
