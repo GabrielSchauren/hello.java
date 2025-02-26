e9b9c69281d8f0b16cb75559b6e263f063642384
GabrielSchauren <gajschauren@minha.fag.edu.br>
2025-02-26 01:22:38 -0300

    criado o hello world

---
+++ main.java
@@ -0,0 +1,8 @@
+public class main {
+    public static void main(String[] args) {
+        HelloWorld();
+    }
+    public static void HelloWorld() {
+        System.out.println("Hello World!");
+    }
+}
