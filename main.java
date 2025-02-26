0b66591b338ae98c10c8dbf47158dabe0553fde1
GabrielSchauren <gajschauren@minha.fag.edu.br>
2025-02-26 01:26:02 -0300

    criado o hola que tal

--- main.java
+++ main.java
@@ -1,8 +1,12 @@
 public class main {
     public static void main(String[] args) {
         HelloWorld();
+        holaquetal();
     }
     public static void HelloWorld() {
         System.out.println("Hello World!");
+    }
+    public static void holaquetal() {
+        System.out.println("Hola que tal?");
     }
 }
